ba40f7f25c78a3dc829bc75d959e08a6dd8297e5
Visual-Studio-Coder <vaibhavguy39@gmail.com>
2024-03-08 10:58:49 -0800

    History is now Library

---
+++ QRCodeShareSheet/Haptics.swift
@@ -0,0 +1,23 @@
+//
+//  Haptics.swift
+//  QRCodeShareSheet
+//
+
+//
+
+import Foundation
+import UIKit
+
+class Haptics {
+    static let shared = Haptics()
+    
+    private init() { }
+
+    func play(_ feedbackStyle: UIImpactFeedbackGenerator.FeedbackStyle) {
+        UIImpactFeedbackGenerator(style: feedbackStyle).impactOccurred()
+    }
+    
+    func notify(_ feedbackType: UINotificationFeedbackGenerator.FeedbackType) {
+        UINotificationFeedbackGenerator().notificationOccurred(feedbackType)
+    }
+}
