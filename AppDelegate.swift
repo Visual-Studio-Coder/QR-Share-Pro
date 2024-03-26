15df2b6cea53caee4e2799da222765453f106317
Visual-Studio-Coder <vaibhavguy39@gmail.com>
2024-03-25 21:11:07 -0700

    Add quick actions to app icon

---
+++ AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  QRCodeShareSheet
+//
+//  Created by   on 3/25/24.
+//
+
+import UIKit
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        let sceneConfiguration = UISceneConfiguration(name: "Default", sessionRole: connectingSceneSession.role)
+        sceneConfiguration.delegateClass = SceneDelegate.self
+        return sceneConfiguration
+    }
+}
