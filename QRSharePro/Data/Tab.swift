84350bb6d865f155be00e38468a2f48eb72e22ce
Aaron <0d1y6g0w@duck.com>
2024-04-06 12:45:48 -0700

    Branding changes + Home.swift improvements

---
+++ QRSharePro/Data/Tab.swift
@@ -0,0 +1,14 @@
+//
+//  Tab.swift
+//  QRSharePro
+//
+//  Created by   on 4/3/24.
+//
+
+import Foundation
+
+enum Tab: String, CaseIterable {
+    case Scanner
+    case NewQRCode
+    case History
+}
