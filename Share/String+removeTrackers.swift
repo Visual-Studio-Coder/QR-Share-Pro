19d49a9d4ca700ae4cdde0272f25cedd2c5ff308
Visual-Studio-Coder <vaibhavguy39@gmail.com>
2024-04-07 14:29:25 -0700

    Permissions button, remove URL trackers, scanner improvements, bug fixes + prepare for release

--- Share/String+removeTrackers.swift
+++ Share/String+removeTrackers.swift
@@ -1,6 +1,6 @@
 //
-//  String+isValidURL.swift
-//  QRSharePro
+//  String+removeTrackers.swift
+//  Share
 //
 
 //
@@ -8,17 +8,6 @@
 import Foundation
 
 extension String {
-    func isValidURL() -> Bool {
-        if let url = URLComponents(string: self) {
-            if url.scheme != nil && !url.scheme!.isEmpty {
-                let scheme = (url.scheme ?? "fail")
-                return scheme == "http" || scheme == "https"
-            }
-        }
-
-        return false
-    }
-
     func removeTrackers() -> String {
         var components = URLComponents(url: URL(string: self)!, resolvingAgainstBaseURL: true)!
         
