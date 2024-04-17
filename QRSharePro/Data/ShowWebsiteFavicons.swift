74fb7c3dc64eec45db98e59979923457305b4067
Aaron <0d1y6g0w@duck.com>
2024-04-17 11:59:14 -0700

    New opt-out from DuckDuckGo website favicons

---
+++ QRSharePro/Data/ShowWebsiteFavicons.swift
@@ -0,0 +1,12 @@
+//
+//  ShowWebsiteFavicons.swift
+//  QRSharePro
+//
+//  Created by   on 4/17/24.
+//
+
+import Foundation
+
+enum ShowWebsiteFavicons {
+    static let showWebsiteFavicons = true
+}
