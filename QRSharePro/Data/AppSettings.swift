de6b3e9eca754123d847446e17c7f9958c1fbe8c
Visual-Studio-Coder <vaibhavguy39@gmail.com>
2024-04-20 13:04:56 -0700

    Efficiency improvements + re-format code

---
+++ QRSharePro/Data/AppSettings.swift
@@ -0,0 +1,14 @@
+//
+//  AppSettings.swift
+//  QRSharePro
+//
+
+//
+
+import Foundation
+
+enum AppSettings {
+    static let showWebsiteFavicons = true
+    static let playHaptics = true
+    static let launchTab: Tab = .NewQRCode
+}
