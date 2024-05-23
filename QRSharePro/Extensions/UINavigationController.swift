8e102fa04e6222ee6367421a60ac5c0b183983df
Visual-Studio-Coder <vaibhavguy39@gmail.com>
2024-05-23 16:41:24 -0700

    App build # + back button in setting sheet improvements

---
+++ QRSharePro/Extensions/UINavigationController.swift
@@ -0,0 +1,19 @@
+//
+//  UINavigationController.swift
+//  QRSharePro
+//
+
+//
+
+import UIKit
+
+extension UINavigationController: UIGestureRecognizerDelegate {
+    open override func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = self
+    }
+    
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return viewControllers.count > 1
+    }
+}
--- QRSharePro/Extensions/View.swift
+++ QRSharePro/Extensions/View.swift
@@ -1,7 +1,18 @@
+//
+//  View.swift
+//  QRSharePro
+//
+
+//
+
 import SwiftUI
 
 extension View {
     func splashView<SplashContent: View>(@ViewBuilder splashContent: @escaping () -> SplashContent) -> some View {
         self.modifier(SplashView(splashContent: splashContent))
+    }
+    
+    func navigationBackButton(color: Color, text: String) -> some View {
+        modifier(NavigationBackButton(color: color, text: text))
     }
 }
