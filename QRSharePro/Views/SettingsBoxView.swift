969d3bad1bcfffabbb3ea1e01f16c73350049734
Visual-Studio-Coder <vaibhavguy39@gmail.com>
2024-08-24 16:34:55 -0700

    Settings sheet re-design

---
+++ QRSharePro/Views/SettingsBoxView.swift
@@ -0,0 +1,31 @@
+//
+//  SettingsBoxView.swift
+//  QRSharePro
+//
+
+//
+
+
+import SwiftUI
+
+struct SettingsBoxView: View {
+	var icon: String
+	var style: Color = .white
+	var color: Color
+	
+	var body: some View {
+		Image(systemName: icon)
+			.resizable()
+			.scaledToFit()
+			.frame(width: 20, height: 20)
+			.padding()
+			.background(color)
+			.foregroundStyle(style)
+			.frame(width: 30, height: 30)
+			.clipShape(RoundedRectangle(cornerRadius: 6))
+	}
+}
+
+#Preview {
+	SettingsBoxView(icon: "cpu", color: .pink)
+}
